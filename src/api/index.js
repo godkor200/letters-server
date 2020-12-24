62c7c7561a608f3980e65a4adaa49caa497488fb
YUBYEONGGUK <godkor200@gmail.com>
2020-12-24 22:26:18 +0900

    get routing

--- src/api/index.js
+++ src/api/index.js
@@ -1,8 +1,10 @@
 const Router = require("koa-router");
 const letters = require("./Letters/index");
+const comments = require("./Comments/index");
 
 const api = new Router();
 
+api.use("/comments", comments.routes());
 api.use("/letters", letters.routes());
 
 module.exports = api;
